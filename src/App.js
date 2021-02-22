e5b4ab6f32758c968dc1d82882e6d9372b94f2f7
leigh90 <zawadikibathi@gmail.com>
2021-02-22 14:26:00 +0300

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Google Clone</h1>  
+      <div className="App">
+        {/* <h1>Google Clone</h1> */}
 
-    </div>
+        <Router>
+          <Switch>
+            <Route path="/search">
+                {/* SearchPage */}
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </Router>
+
+      </div>
+   
   );
 }
 
